db500fd1f607046c61a6134c51d4e4400a8d6201
Junior Alves <jrnalves@gmail.com>
2022-06-28 16:49:46 -0300

    create input Text

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,78 @@
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import { Container, Typography, Box, Stack, TextField } from '@mui/material';
+import { docPessoal, estadoCivil, endereco, empresa, referencias, banco } from '../src/inputText'
+
+const boxStyle = { p: 2, border: '1px solid #c4c4c4', borderRadius: '10px' }
+
+const createInputText = (props) => {
+  return (
+    props.map((item, index) => (
+      <TextField
+        key={index}
+        id={item.id}
+        name={item.name}
+        label={item.label}
+        placeholder={item.placeholder}
+        required={item.required}
+        variant="standard"
+        fullWidth
+        margin="normal"
+      />
+    ))
+  )
+}
+
 
 export default function Index() {
   return (
-    <Container maxWidth="sm">
+    <Container maxWidth="md">
       <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Formulário Blockton
+        <Typography variant="h4" component="h1" gutterBottom align='center' color="primary.main">
+          Formulário da Blockton
         </Typography>
+
+        <Stack component="form" spacing={4}>
+
+          <Box sx={boxStyle}>
+            {createInputText(docPessoal)}
+          </Box>
+
+          <Box sx={boxStyle}>
+            <Typography variant="h4" component="h1" gutterBottom color="secondary.main">
+              Estado Civil:
+            </Typography>
+            {createInputText(estadoCivil)}
+          </Box>
+
+          <Box sx={boxStyle}>
+            <Typography variant="h4" component="h1" gutterBottom color="secondary.main">
+              Endereço:
+            </Typography>
+            {createInputText(endereco)}
+          </Box>
+
+          <Box sx={boxStyle}>
+            <Typography variant="h4" component="h1" gutterBottom color="secondary.main">
+              Dados da Empresa:
+            </Typography>
+            {createInputText(empresa)}
+          </Box>
+
+          <Box sx={boxStyle}>
+            <Typography variant="h4" component="h1" gutterBottom color="secondary.main">
+              Referências Pessoais:
+            </Typography>
+            {createInputText(referencias)}
+          </Box>
+
+          <Box sx={boxStyle}>
+            <Typography variant="h4" component="h1" gutterBottom color="secondary.main">
+              Dados do Banco:
+            </Typography>
+            {createInputText(banco)}
+          </Box>
+
+        </Stack>
+
       </Box>
     </Container>
   );
---
+++ src/inputText.js
@@ -0,0 +1,201 @@
+export const docPessoal = [
+  {
+    type: "text",
+    id: "name",
+    name: "Nome",
+    label: "Nome",
+    placeholder: "Nome Completo",
+    required: true
+  },
+  {
+    type: "text",
+    id: "cpf",
+    name: "CPF",
+    label: "CPF",
+    placeholder: "000.000.000-00",
+    required: true
+  },
+  {
+    type: "text",
+    id: "celular",
+    name: "Celular",
+    label: "Celular",
+    placeholder: "(xx) xxxxx-xxxx",
+    required: true
+  },
+  {
+    type: "text",
+    id: "telefone",
+    name: "Telefone",
+    label: "Telefone",
+    placeholder: "(xx) xxxxx-xxxx",
+    required: false
+  },
+  {
+    type: "email",
+    id: "email",
+    name: "Email",
+    label: "Email",
+    placeholder: "endereco@dominio.com.br",
+    required: false
+  },
+]
+
+export const estadoCivil = [
+  {
+    type: "text",
+    id: "nameConjuge",
+    name: "Nome Conjuge",
+    label: "Nome completo do esposo(a)",
+    placeholder: "",
+    required: false
+  },
+  {
+    type: "text",
+    id: "dataConjuge",
+    name: "Nascimento Conjuge",
+    label: "Data de nascimento do esposo(a)",
+    placeholder: "dd/mm/aaaa",
+    required: false
+  },
+]
+
+export const endereco = [
+  {
+    type: "text",
+    id: "cep",
+    name: "CEP",
+    label: "CEP",
+    placeholder: "",
+    required: false
+  },
+  {
+    type: "text",
+    id: "endereco",
+    name: "Endereço",
+    label: "Endereço",
+    placeholder: "",
+    required: false
+  },
+  {
+    type: "text",
+    id: "bairro",
+    name: "Bairro",
+    label: "Bairro",
+    placeholder: "",
+    required: false
+  },
+  {
+    type: "text",
+    id: "city",
+    name: "Cidade",
+    label: "Cidade",
+    placeholder: "",
+    required: false
+  },
+  {
+    type: "text",
+    id: "estado",
+    name: "Estado",
+    label: "estado",
+    placeholder: "Rua",
+    required: false
+  },
+]
+
+export const empresa = [
+  {
+    type: "text",
+    id: "empresa",
+    name: "Empresa",
+    label: "Nome da Empresa atual ou profisão",
+    placeholder: "Informe a empresa que você trabalha, em caso de autonomo, informe sua profissão",
+    required: false
+  },
+  {
+    type: "text",
+    id: "telefoneComercial",
+    name: "Telefone Comercial",
+    label: "Telefone Comercial",
+    placeholder: "(xx) xxxxx-xxxx",
+    required: false
+  },
+  {
+    type: "text",
+    id: "dataAdmissao",
+    name: "Data de admissão",
+    label: "Data de admissão",
+    placeholder: "dd/mm/aaaa",
+    required: false
+  },
+  {
+    type: "text",
+    id: "salario",
+    name: "Salario",
+    label: "Salario",
+    placeholder: "R$0,00",
+    required: true
+  },
+]
+
+export const referencias = [
+  {
+    type: "text",
+    id: "referencia1",
+    name: "Referencia Pessoal 1",
+    label: "Nome de Referência 1",
+    placeholder: "",
+    required: true
+  },
+  {
+    type: "text",
+    id: "foneReferencia1",
+    name: "Telefone refencia 1",
+    label: "Telefone de Referência 1",
+    placeholder: "(xx) xxxxx-xxxx",
+    required: true
+  },
+  {
+    type: "text",
+    id: "referencia2",
+    name: "Referencia Pessoal 2",
+    label: "Nome de Referência 2",
+    placeholder: "",
+    required: true
+  },
+  {
+    type: "text",
+    id: "foneReferenci2",
+    name: "Telefone de Referência 2",
+    label: "Telefone de Referência 2",
+    placeholder: "(xx) xxxxx-xxxx",
+    required: true
+  },
+]
+
+export const banco = [
+  {
+    type: "text",
+    id: "banco",
+    name: "Nome do banco",
+    label: "Nome do Banco",
+    placeholder: "",
+    required: false
+  },
+  {
+    type: "text",
+    id: "agencia",
+    name: "Agência",
+    label: "Agência",
+    placeholder: "",
+    required: false
+  },
+  {
+    type: "text",
+    id: "conta",
+    name: "Conta",
+    label: "Conta",
+    placeholder: "",
+    required: false
+  },
+]
